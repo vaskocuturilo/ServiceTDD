dd707e156cbea8e0be8898c457bfb97fd4b1d197
asm <vaskocuturilo@rambler.ru>
2023-04-26 20:14:38 +0200

    QA-00004: Add unit test for update functionality.

--- src/test/java/com/example/servicetdd/service/UserServiceTest.java
+++ src/test/java/com/example/servicetdd/service/UserServiceTest.java
@@ -139,4 +139,24 @@ class UserServiceTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$[*]").isEmpty())
                 .andExpect(MockMvcResultMatchers.content().string("A user was delete"));
     }
+
+    @Test
+    public void givenUser_whenUpdateUser_thenStatus200() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .put("/api/v1/users")
+                        .content(new ObjectMapper().writeValueAsString(new UserEntity(1L, "test1@test.com", "password")))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.username").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.password").exists())
+
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").isNotEmpty())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.username").isNotEmpty())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.password").isNotEmpty())
+
+                .andExpect(MockMvcResultMatchers.jsonPath("$[*]", hasSize(greaterThanOrEqualTo(1))))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.username", equalTo("test1@test.com")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.password", equalTo("password")));
+    }
 }
